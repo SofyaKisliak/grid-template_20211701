c5f2bd0666a75fe0205e1d01ae673f14e8bd511f
Sofya Kisliak <sofkisliak@gmail.com>
2021-04-13 19:30:20 +0300

    перенос кода css в scss и структурирование файлов

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,8 @@
 
-$(document).ready(function () {
-
-  $('.header-navbar__hamburger').on('click', function () {
-    $(this).toggleClass('active');
+$(document).ready(function(){
+	$('.header-navbar__btn').on('click', function(){
+		$(this).toggleClass('active');
     $('.header-navbar__list').toggleClass('active');
-  });
-
+	});
 });
+
