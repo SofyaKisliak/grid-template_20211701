1181be87524b1b68cc48b150f0efd96b57cbb1d9
Sofya Kisliak <sofkisliak@gmail.com>
2021-04-16 01:05:02 +0300

    responsiv md-sm, md-min, sm-min

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,15 @@
 
-$(document).ready(function(){
+/*$(document).ready(function(){
 	$('.header-navbar__btn').on('click', function(){
 		$(this).toggleClass('active');
     $('.header-navbar__list').toggleClass('active');
+	});
+});*/
+
+$(document).ready(function(){
+	$('.header-navbar__btn').on('click', function(){
+		$(this).toggleClass('active');
+    $('.header-navbar__list').stop(true, true).slideToggle(500);
 	});
 });
 
