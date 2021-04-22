64dd660722232bf8782d62e0eb4cdfd01f50159c
Sofya Kisliak <sofkisliak@gmail.com>
2021-04-22 13:21:37 +0300

    исправление ошибок , валидация css кода, placeholder (form), кнопка меню, выпадающий список

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,10 +6,19 @@
 	});
 });*/
 
-$(document).ready(function(){
+/*$(document).ready(function(){
 	$('.header-navbar__btn').on('click', function(){
 		$(this).toggleClass('active');
-    $('.header-navbar__list').stop(true, true).slideToggle(500);
+    $('.header-navbar__list').stop(true,true).slideToggle('active');
 	});
-});
+});*/
 
+$(document).ready(function () {
+
+	$('.header-navbar__btn').on('click', function () {
+	  $(this).toggleClass('active');
+	  $('.header-navbar__list').toggleClass('active');
+	  //$('.active').toggleClass('active');
+	});
+  
+  });
